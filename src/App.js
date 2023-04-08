f14c8788b1330ae5d10af8ac961bc7d88ad717ea
Rodrigo Vazquez <rodrigo_vazquez_99@hotmail.com>
2023-04-08 16:37:35 -0300

    creando nuevas rutas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Inicio from "./components/Inicio";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -9,24 +9,34 @@ import Locales from "./components/Locales";
 import Error404 from "./components/Error404";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
+import CartContextProvider from "./components/context/CartContext";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
-    <section >
-      <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Inicio/>}/>
-        <Route path="/menu" element={<ContainerMenu/>}/>
-        <Route path="/menu/:menuId" element={<ItemListContainer/>}/>
-        <Route path="/menu/:menuId/:id" element={<ItemDetailContainer/>}/>        
-        <Route path="/promociones" element={<Promociones/>}/>
-        <Route path="/locales" element={<Locales/>}/>
-        <Route path="*" element={<Error404/>}/>
-      </Routes>
-      <Footer />
-      </BrowserRouter>
-    </section>
+    
+      <CartContextProvider>
+        <div >
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/menu" element={<ContainerMenu />} />
+            <Route path="/menu/:menuId" element={<ItemListContainer />} />
+            <Route path="/menu/:menuId/:id" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/promociones" element={<Promociones />} />
+            <Route path="/locales" element={<Locales />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+        </div>
+      </CartContextProvider>
+
+    
   );
 }
 
