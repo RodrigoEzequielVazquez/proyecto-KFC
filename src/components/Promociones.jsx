92ccb6dfa7a5c54d2c209b4c33102e9471e5071e
Rodrigo Vazquez <rodrigo_vazquez_99@hotmail.com>
2023-04-10 21:30:10 -0300

    dando funcionalidad y estilos a la pagina de promociones

--- src/components/Promociones.jsx
+++ src/components/Promociones.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/promociones.css"
-import { Link } from "react-router-dom"
-
+import arrayLocales from "./json/arrayLocales.json"
+import promos from "./json/promo.json"
 
 const Promociones = () => {
 
-    function promoDetails() {
-        console.log("click");
+    const expresiones = {
+        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    }
+
+    const [nombrePromo, setNombrePromo] = useState("")
+    const [descuento, setDescuento] = useState("")
+    const [email, setEmail] = useState(false)
+    const [modalBody, setModalBody] = useState("Ha ocurrido un error solicitando el voucher.Intentalo mas tarde. Recuerda que si ya solicitaste un cupon antes no podras solicitar otro")
+
+    const promoInfo = (nombre, descuento) => {
+        setDescuento(descuento)
+        setNombrePromo(nombre)
+    }
+
+    const validarMail = (e) => {
+        setEmail(true)
+        if (expresiones.correo.test(e.target.value)) {
+            setModalBody("La solicitud de la promocion fue exitosa. Pronto recibiras el codigo en " + e)
+        }
 
     }
 
@@ -19,29 +36,64 @@ const Promociones = () => {
             </div>
 
             <div id="containerCupones">
-
-                <div className="boxCupon ">
-                    <Link to="" onClick={promoDetails}>
-                        <img src="https://kfc.com.ar/images/promociones/1632931492KFC.jpg" alt="" />
-                    </Link>
-                </div>
-
-                <div className="boxCupon">
-                    <Link to="" onClick={promoDetails}>
-                        <img src="https://kfc.com.ar/images/promociones/1675965545Cup%C3%B3n%20Web%20-%20Kreamball%201.jpg" alt="" />
-                    </Link>
-                </div>
-
-                <div className="boxCupon">
-                    <Link to="" onClick={promoDetails}>
-                        <img src="https://kfc.com.ar/images/promociones/1675965561Cup%C3%B3n%20Web%20-%20Tostado.jpg" alt="" />
-                    </Link>
-                </div>
+                {
+                    promos.map((promo) =>
+                        <div className="boxCupon" key={promo.nombre}>
+                            <a href="" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => promoInfo(promo.nombre, promo.descuento)}>
+                                <img src={promo.img} alt={promo.nombre} />
+                            </a>
+                        </div>
+                    )
+                }
 
             </div >
 
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="modalTitle" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="modalTitle">Solicitud de promo: {nombrePromo}</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <h3>{descuento}{nombrePromo}</h3>
+                            <h3>Detalles de la promocion</h3>
+                            <h3>Promocion vigente a partir del 28-09-2021.Vence el 31-12-2024</h3>
+                            <h3>Domingos-Lunes-Martes-Miercoles-Jueves-Viernes-Sabados.</h3>
+                            <ul>
+                                {arrayLocales.map(locales => <li key={locales.id}>{locales.localidad}</li>)}
+                            </ul>
+                            <div id="inputPromo">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input type="email" className="form-control" placeholder="Ingrese su email" onInput={(e) => { validarMail(e) }} />
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            {
+                                email ? <button className="btn btn-danger" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" >Solicitar</button> : <button className="btn btn-danger">Solicitar</button>
+                            }
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabIndex="-1">
+                <div className="modal-dialog modal-dialog-top">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalToggleLabel2">Solicitud de promo:</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            {modalBody}
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     )
 }
 
