4743fc269642dc9c9f3ead372c44f59982e2b94a
Rodrigo Vazquez <rodrigo_vazquez_99@hotmail.com>
2023-04-10 23:21:12 -0300

    definiendo rutas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../css/footer.css';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
@@ -11,33 +12,32 @@ const Footer = () => {
             <div className="infoFooter">
                 <div>
                     <p>Locales</p>
-                    <a href="">Buscar KFC</a>
+                    <Link to="/locales">Buscar KFC</Link>
                 </div>
                 <div>
                     <p>Comunicacion</p>
-                    <a href="">Contacto</a>
+                    <Link to="/">Contacto</Link>
                 </div>
                 <div>
                     <p>RRHH</p>
-                    <a href="">Trabaja con nosotros</a>
+                    <Link to="https://docs.google.com/forms/d/e/1FAIpQLSeYhkmz5ve7AmDhAWN93G1UXN2vz4LZYkWtj-MqrDJ3NjhdXA/viewform" target={"_blank"} rel="noreferrer">Trabaja con nosotros</Link>
                 </div>
             </div>
             <div className="containerRedesTerminos">
                 <div className="redesFooter">
-                    <a href="https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2FKFCArgentina%2Fapp%2F137541772984354%2F" target={"_blank"} rel="noreferrer"><img className="facebook" src="icons/facebook.png" alt="" /></a>
+                    <Link to="https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2FKFCArgentina%2Fapp%2F137541772984354%2F" target={"_blank"} rel="noreferrer"><img className="facebook" src="icons/facebook.png" alt="" /></Link>
 
-                    <a href="https://twitter.com/KFC_Argentina" target={"_blank"} rel="noreferrer"><img src="icons/twitter.svg" alt="" /></a>
+                    <Link to="https://twitter.com/KFC_Argentina" target={"_blank"} rel="noreferrer"><img src="icons/twitter.svg" alt="" /></Link>
 
-                    <a href="https://www.instagram.com/kfc_argentina/" target={"_blank"} rel="noreferrer"><img src="icons/instagram.svg" alt="" /></a>
+                    <Link to="https://www.instagram.com/kfc_argentina/" target={"_blank"} rel="noreferrer"><img src="icons/instagram.svg" alt="" /></Link>
 
-                    <a href="https://www.youtube.com/channel/UCFVMW8vKJP_DQd2F0ZIBIfQ" target={"_blank"} rel="noreferrer"><img src="icons/youTube.svg" alt="" /></a>
-
+                    <Link to="https://www.youtube.com/channel/UCFVMW8vKJP_DQd2F0ZIBIfQ" target={"_blank"} rel="noreferrer"><img src="icons/youTube.svg" alt="" /></Link>
 
                 </div>
 
                 <div className="terminosFooter">
-                    <a href="">Terminos</a>
-                    <a href="">Privacidad</a>
+                    <Link to="/terminos">Terminos</Link>
+                    <Link to="/privacidad">Privacidad</Link>
                     <p>KFC Argentina - 2018</p>
                 </div>
             </div>
