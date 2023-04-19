8a5973285a1819a2bfc8e0f6f01cf68e855c7c41
Rodrigo Vazquez <rodrigo_vazquez_99@hotmail.com>
2023-04-18 21:42:19 -0300

    creando codigo con tabla responsive en el carrito

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from "react";
 import { CartContext } from "./context/CartContext";
 import { Link } from "react-router-dom";
 import '../css/contenedores.css';
+import '../css/cart.css';
 
 const Cart = () => {
     const { cart, removeItem, clear, sumTotal, cartTotal } = useContext(CartContext)
 
     if (cartTotal() === 0) {
         return (
-            <div id="aumentarContenedor"> 
+            <div id="aumentarContenedor">
                 <div className="row">
                     <div className="col-md-12 text-center">
                         <div className="alert alert-danger" role="alert">No se encontraron productos en el carrito</div>
@@ -19,46 +20,80 @@ const Cart = () => {
         )
     }
 
-    return(
-    <div className="container py-5">
-        <div className="row">
-            <div className="col-md-12">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">&nbsp;</th>
-                            <th scope="col">Producto</th>
-                            <th scope="col" className="text-center">Cantidad</th>
-                            <th scope="col" className="text-center">Precio</th>
-                            <th scope="col">&nbsp;</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {cart.map(item => (
-                            <tr key={item.id}>
-                                <td><img src={item.imagen} alt={item.nombre} width={60} /></td>
-                                <td className="align-middle">{item.nombre}</td>
-                                <td className="text-center align-middle">{item.quantity}</td>
-                                <td className="text-center align-middle">{item.quantity * item.precio}</td>
-                                <td className="text-end align-middle"><Link onClick={() => { removeItem(item.id) }}><img src={"icons/trash3-fill.svg"} alt={"Eliminar Producto"} width={25} title="Eliminar el producto" /></Link></td>
+    return (
+        <div className="container py-5">
+            <div className="row">
+                <div className="col-md-12">
+                    <table className="table" id="principalCart">
+                        <thead>
+                            <tr>
+                                <th scope="col">&nbsp;</th>
+                                <th scope="col">Producto</th>
+                                <th scope="col" className="text-center">Cantidad</th>
+                                <th scope="col" className="text-center">Precio</th>
+                                <th scope="col">&nbsp;</th>
                             </tr>
-                        ))}
-                        <tr>
-                            <td colSpan={2}>&nbsp;</td>
-                            <td className="text-center">Total</td>
-                            <td className="text-center"><b>${sumTotal()}</b></td>
-                            <td>&nbsp;</td>
-                        </tr>
-                        <tr>
-                            <th scope="col" colSpan={3} className="text-center"><Link onClick={clear} className="btn btn-outline-danger" title="Vaciar carrito">Vaciar carrito
-                            </Link></th>
-                            <th><Link to={"/checkout"} className="btn btn-outline-danger" title="Finalizar compra">Finalizar compra</Link></th>
-                        </tr>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {cart.map(item => (
+                                <tr key={item.id}>
+                                    <td><img src={item.imagen} alt={item.nombre} width={60} /></td>
+                                    <td className="align-middle">{item.nombre}</td>
+                                    <td className="text-center align-middle">{item.quantity}</td>
+                                    <td className="text-center align-middle">{item.quantity * item.precio}</td>
+                                    <td className="text-end align-middle"><Link onClick={() => { removeItem(item.id) }}><img src={"icons/trash3-fill.svg"} alt={"Eliminar Producto"} width={25} title="Eliminar el producto" /></Link></td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <td colSpan={2}>&nbsp;</td>
+                                <td className="text-center">Total</td>
+                                <td className="text-center"><b>${sumTotal()}</b></td>
+                                <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <th colSpan={2} className="text-center"><Link onClick={clear} className="btn btn-outline-danger" title="Vaciar carrito">Vaciar carrito
+                                </Link></th>
+                                <th colSpan={3} className="text-center" ><Link to={"/checkout"} className="btn btn-outline-danger" title="Finalizar compra">Finalizar compra</Link></th>
+                            </tr>
+                        </tbody>
+                    </table>
+                    
+                    <table className="table" id="responsiveCart">
+                        <thead>
+                            <tr>
+                                <th scope="col">Producto</th>
+                                <th scope="col">Cantidad</th>
+                                <th scope="col">Precio</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cart.map(item => (
+                                <tr key={item.id}>
+
+                                    <td className="align-middle">{item.nombre}</td>
+                                    <td className=" align-middle">{item.quantity}</td>
+                                    <td className=" align-middle">{item.quantity * item.precio}</td>
+                                    <td className="align-middle"><Link onClick={() => { removeItem(item.id) }}><img src={"icons/trash3-fill.svg"} alt={"Eliminar Producto"} width={25} title="Eliminar el producto" /></Link></td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <td colSpan={1}>&nbsp;</td>
+                                <td>Total</td>
+                                <td><b>${sumTotal()}</b></td>
+                                <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <th colSpan={1} className="align-middle text-center"><Link onClick={clear} className="btn btn-outline-danger" title="Vaciar carrito">Vaciar carrito
+                                </Link></th>
+                                <th colSpan={2} className="align-middle text-center"><Link to={"/checkout"} className="btn btn-outline-danger" title="Finalizar compra">Finalizar compra</Link></th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
+
+
     )
 }
 
