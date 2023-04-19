111c71d3d990d69cc95c56c12ffa5667d97a537b
Rodrigo Vazquez <rodrigo_vazquez_99@hotmail.com>
2023-04-18 21:59:16 -0300

    eliminando etiqueta b en el nombre

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import "../css/items.css"
 
 const Item = ({ item }) => {
 
@@ -8,7 +9,7 @@ const Item = ({ item }) => {
             <div className="card">
                 <img src={item.imagen} className="card-img-top" height={250} alt={item.nombre} />
                 <div className="card-body">
-                    <p className="card-text">{item.nombre}</p>
+                    <h4 className="card-text">{item.nombre}</h4>
                     <p> {item.descripcion}</p>
                 </div>
             </div>
